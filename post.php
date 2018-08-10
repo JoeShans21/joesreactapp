46e62260cc99bc841e95120909d23b1fd78ea63d
Joe Shanahan <shans.joe@gmail.com>
2018-08-09 21:22:28 -0400

    gch

---
+++ post.php
@@ -0,0 +1,17 @@
+<?php
+	try {
+		$conn=mysqli_connect("sql9.freemysqlhosting.net", "sql9251151", "w73z9SwvYP", "sql9251151");
+		$Content = $_POST['Content'];
+		$Author = $_POST['Author'];
+		$sql = "INSERT INTO mobile_messages (Content, Author) VALUES ('$Content', '$Author', 'n');";
+    	mysqli_query($conn, $sql);
+		echo json_encode(array(
+        	"sendback" => 'Your message has been posted',
+    	));
+	}
+	catch ($err) {
+		echo json_encode(array(
+        	"sendback" => $err,
+    	));
+	}
+?>
