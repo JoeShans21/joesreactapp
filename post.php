ae6e4e6ef92807e0637f0755db418afca59acc6f
Joe Shanahan <shans.joe@gmail.com>
2018-08-09 21:31:12 -0400

    fgh

--- post.php
+++ post.php
@@ -1,17 +1,10 @@
 <?php
-	try {
-		$conn=mysqli_connect("sql9.freemysqlhosting.net", "sql9251151", "w73z9SwvYP", "sql9251151");
-		$Content = $_POST['Content'];
-		$Author = $_POST['Author'];
-		$sql = "INSERT INTO mobile_messages (Content, Author) VALUES ('$Content', '$Author', 'n');";
-    	mysqli_query($conn, $sql);
-		echo json_encode(array(
-        	"sendback" => 'Your message has been posted',
-    	));
-	}
-	catch ($err) {
-		echo json_encode(array(
-        	"sendback" => $err,
-    	));
-	}
+	$conn=mysqli_connect("sql9.freemysqlhosting.net", "sql9251151", "w73z9SwvYP", "sql9251151");
+	$Content = $_POST['Content'];
+	$Author = $_POST['Author'];
+	$sql = "INSERT INTO mobile_messages (Content, Author) VALUES ('$Content', '$Author', 'n');";
+    mysqli_query($conn, $sql);
+	echo json_encode(array(
+        "sendback" => 'Your message has been posted',
+    ));
 ?>
