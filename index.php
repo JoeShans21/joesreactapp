b7d89691322b40415e7bbe556ab082e7806d1de2
Joe Shanahan <shans.joe@gmail.com>
2018-08-09 20:28:20 -0400

    gvdfgfgh

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
+	$arr = []
+	$conn = mysqli_connect("sql9.freemysqlhosting.net", "sql9251151", "w73z9SwvYP", "sql9251151");
+	$sql="SELECT * FROM mobile_messages";
+    include_once 'includes/dbh.inc.php';
+    $result=mysqli_query($conn,$sql);
+    $rowcount=mysqli_num_rows($result);
+    for ($x=$rowcount; $x>=1; $x--){
+        $sql2="SELECT * FROM posts WHERE ID='$x'";
+        $result=mysqli_fetch_assoc(mysqli_query($conn,$sql2));
+        $content=$result['Content'];
+        $author=$result['Author'];
+		array_push($arr, [$content, $author]);
+    }
     echo json_encode(array(
-        "message" => "The message has been returned from backend",
+        "message" => $arr,
     ));
 ?>
