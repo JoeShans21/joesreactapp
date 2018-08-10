bb350293bc6604be67c50d3d79df83710d91f70e
Joe Shanahan <shans.joe@gmail.com>
2018-08-09 21:43:30 -0400

    get

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <?php
 	$conn=mysqli_connect("sql9.freemysqlhosting.net", "sql9251151", "w73z9SwvYP", "sql9251151");
-	$Content = $_POST['Content'];
-	$Author = $_POST['Author'];
+	$Content = $_GET['Content'];
+	$Author = $_GET['Author'];
 	$sql = "INSERT INTO mobile_messages (Content, Author) VALUES ('$Content', '$Author');";
     mysqli_query($conn, $sql);
 	echo json_encode(array(
